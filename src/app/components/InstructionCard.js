9b4e07052ddeb621669e5500ffc897b28f53d557
aryapatxl <aryaxrp@gmail.com>
2024-10-26 00:01:50 -0700

    added arrow

--- src/app/components/InstructionCard.js
+++ src/app/components/InstructionCard.js
@@ -4,7 +4,7 @@ import React from "react";
 const InstructionCard = ({ stepNumber, title, subtitle }) => (
   <div className="card-size">
     <div className="py-10 pl-12 flex items-center bg-blue dark:bg-blue-dark">
-      <h1 className="text-7xl sm:text-15xl font-mono text-white dark:text-white-dark mr-7">{stepNumber}</h1>
+      <h1 className="text-7xl my-7 sm:text-15xl font-mono text-white dark:text-white-dark mr-2">{stepNumber}</h1>
       <div className="flex flex-col">
         <p className="font-con font-bold text-white dark:text-white-dark text-3xl sm:text-7xl mb-2">{title}</p>
         <p className="font-con font-bold text-white dark:text-white-dark text-3xl sm:text-7xl">{subtitle}</p>
--- src/app/page.js
+++ src/app/page.js
@@ -43,6 +43,22 @@ const HomePage = () => {
           UNC<span style={{ display: 'inline-block', transform: 'skewX(-15deg)' }}>O</span>VERED
         </h1>
       </div>
+      <div className="bg-white dark:bg-white-dark">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1}
+          stroke="currentColor"
+          className="ml-10 size-10 bg-white dark:bg-white-dark text-blue dark:text-blue-dark"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+          />
+        </svg>
+      </div>
 
       {/* scrolling instructions section */}
       <div className="bg-blue dark:bg-blue-dark">
